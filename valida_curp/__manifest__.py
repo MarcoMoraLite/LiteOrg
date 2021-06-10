835f1a0f9b38c116152c2eb68db3defd606cd205
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-10 17:31:50 +0000

    Se sube app para validar CURP V2

--- valida_curp/__manifest__.py
+++ valida_curp/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/valida_curp.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
