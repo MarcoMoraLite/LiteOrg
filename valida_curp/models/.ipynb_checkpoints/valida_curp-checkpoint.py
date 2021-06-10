05a3d1984210aac21581807b4c6b71020d35a8ad
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-10 22:21:10 +0000

    V1 INE

--- valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
+++ valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
@@ -10,6 +10,9 @@ class ValidaCurp(models.Model):
 
     curp = fields.Char("CURP")
     response = fields.Text("RESPONSE")
+    cic = fields.Char("cic")
+    identificadorCiudadano = fields.Char("Identificador Ciudadano")
+    response2 = fields.Text("RESPONSE2")
     
     def comprobar(self):
         for record in self:
@@ -22,3 +25,11 @@ class ValidaCurp(models.Model):
             #mapa=json.loads(aux)
             #estatus=mapa["estatus"]
             record.response = r.content
+            
+    def comprobar_ine(self):
+        for record2 in self:
+            cic = record2.cic
+            identificadorCiudadano = record.identificadorCiudadano
+            payload2 ={"cic":cic,"identificadorCiudadano":identificadorCiudadano}
+            r2=requests.post("https://ine.nubarium.com/ine/v2/valida_ine",headers=header,data=json.dumps(payload2))
+            record2.response2 = r2.content
