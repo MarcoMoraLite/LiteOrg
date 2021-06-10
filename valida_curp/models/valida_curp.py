abb111059a6c7b4912965704d47c08c82ac8eefe
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-10 17:05:18 +0000

    Se sube app para validar CURP

---
+++ valida_curp/models/valida_curp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import requests
+import json
+
+class ValidaCurp(models.Model):
+    _name = 'valida_curp.valida_curp'
+    _description = 'valida_curp.valida_curp'
+
+    curp = fields.Char()
+    response = fields.Text()
+    
+    def comprobar(self):
+        curp_usuario = self.curp
+        header = {"Authorization": "Basic bXVsdGlwbGljYTprR19NeC4yeUI5","Content-Type":"application/json"}
+        payload = {"documento":"0","curp":+curp_usuario}
+        r=requests.post("https://curp.nubarium.com/renapo/valida_curp",headers=header,data=json.dumps(payload))
+        #json_response = r.json()
+        #aux=json.dumps(json_response)
+        #mapa=json.loads(aux)
+        #estatus=mapa["estatus"]
+        self.response = r.content
