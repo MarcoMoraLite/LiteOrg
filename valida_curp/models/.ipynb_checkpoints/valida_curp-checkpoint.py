46bc3a58588a565d5f8a63716301330cc6391eb1
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-11 00:51:26 +0000

    V5 cedula

--- valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
+++ valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
@@ -15,7 +15,7 @@ class ValidaCurp(models.Model):
         for record in self:
             cedula_usuario = record.cedula
             header = {"Authorization": "Basic bXVsdGlwbGljYTprR19NeC4yeUI5","Content-Type":"application/json"}
-            payload = {"documento":"0","curp":curp_usuario}
+            payload = {"documento":"0","curp":cedula_usuario}
             r=requests.post("https://curp.nubarium.com/renapo/valida_curp",headers=header,data=json.dumps(payload))
             #json_response = r.json()
             #aux=json.dumps(json_response)
