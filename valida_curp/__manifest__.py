4c8aa6965decdfce18cacd22a392d52cd3566b8a
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-11 00:48:23 +0000

    V4 cedula

--- valida_curp/__manifest__.py
+++ valida_curp/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/valida_cedula.xml',
+        'views/valida_curp.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
