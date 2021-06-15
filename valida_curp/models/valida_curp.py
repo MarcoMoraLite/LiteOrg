71b442951bbc670a772e870ea3a47f37f07fd336
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-15 23:50:39 +0000

    V4 con nuevo codigo

--- valida_curp/models/valida_curp.py
+++ valida_curp/models/valida_curp.py
@@ -29,11 +29,12 @@ class ValidaCurp(models.Model):
     def comprobar2(self):
         for record2 in self:
             ine = record2.ine
-            #ine64 = base64.encodebytes(ine)
+            ine64 = base64.encodebytes(ine)
             #ine64 = base64.b64encode(ine)
             #ine2 = record2.ine_atras
             #ine264 = base64.b64encode(ine2)
             #payload2 = {"id":ine64}
             header2 = {"Authorization": "Basic bXVsdGlwbGljYTprR19NeC4yeUI5","Content-Type":"application/json"}
-            r2=requests.post("https://ine.nubarium.com:443/ocr/obtener_datos",headers=header2,data={"id":ine})
+            r2=requests.post("https://ine.nubarium.com:443/ocr/obtener_datos",headers=header2,data={"id":ine64})
             record2.response2 = r2.content
+            record2.response = ine64
