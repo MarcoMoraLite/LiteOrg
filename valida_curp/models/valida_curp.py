d9191c137b9bb4276cf2374205030310976b9f75
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-15 22:44:28 +0000

    v21 cedula

--- valida_curp/models/valida_curp.py
+++ valida_curp/models/valida_curp.py
@@ -29,10 +29,10 @@ class ValidaCurp(models.Model):
     def comprobar2(self):
         for record2 in self:
             ine = record2.ine
-            ine64 = base64.b64encode(ine)
+            ine64 = base64.b64encode(ine.read())
             #ine2 = record2.ine_atras
             #ine264 = base64.b64encode(ine2)
             payload2 = {"id":ine64}
             header2 = {"Authorization": "Basic bXVsdGlwbGljYTprR19NeC4yeUI5","Content-Type":"application/json"}
             r2=requests.post("https://ine.nubarium.com:443/ocr/obtener_datos",headers=header2,data=json.dumps(payload2))
-            record2.response2 = r2.content.decode('utf-8')
+            record2.response2 = r2.content
