3c63efe0c4d1edf34272fc42518626207f15463a
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-17 23:44:20 +0000

    V1 con reporte

--- valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
+++ valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
@@ -9,7 +9,7 @@ class ValidaCurp(models.Model):
     _name = 'valida_curp.valida_curp'
     _description = 'valida_curp.valida_curp'
     
-    state = fields.Selection([('ine','INE'),('cedula','CEDULA')],'estatus', default='ine')
+    state = fields.Selection([('ine','INE'),('cedula','CEDULA'),('foto','FOTO')],'estatus', default='ine')
     cedula = fields.Char("CEDULA")
     response = fields.Text("RESPUESTA")
     ine = fields.Binary("INE parte delantera")
@@ -24,7 +24,6 @@ class ValidaCurp(models.Model):
             payload = {"numeroCedula":cedula_usuario}
             r=requests.post("https://api.nubarium.com/sep/obtener_cedula",headers=header,data=json.dumps(payload))
             record.response = r.content
-            #record.write({'state': 'cedula'})
             
             
     def comprobar2(self):
@@ -32,14 +31,15 @@ class ValidaCurp(models.Model):
             header2 = {"Authorization": "Basic bTJjcm93ZDpfM2U4dy4wUnMy","Content-Type":"application/json"}
             r2=requests.post("https://ine.nubarium.com:443/ocr/obtener_datos",headers=header2,json={"id":record2.ine})
             record2.response2 = r2.content
-            record2.write({'state': 'cedula'})
             
     def confirmarCed(self):
         for record3 in self:
-            record3.name = "magia"
+            record3.name = "Marco"
+            record3.write({'state': 'foto'})
     
     def confirmarIne(self):
         for record4 in self:
-            record4.apellido = "Ya jalo"
+            record4.apellido = "Mora"
+            record4.write({'state': 'cedula'})
        
             
