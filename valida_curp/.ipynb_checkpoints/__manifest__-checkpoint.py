ec3fe4e99a42a74cc78a4c63bfcaab8a32b0c0c4
MarcoMoraLite <marco.mora@liteorg.com>
2021-06-28 18:58:38 +0000

    V3 para camara

--- valida_curp/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ valida_curp/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['web_image_webcam'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
