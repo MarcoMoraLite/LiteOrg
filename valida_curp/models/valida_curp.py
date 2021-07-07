74087425c3aafde18e90e6940639b9f4a44bc2c8
MarcoMoraLite <marco.mora@liteorg.com>
2021-07-07 17:17:55 +0000

    V1 contacto id

--- valida_curp/models/valida_curp.py
+++ valida_curp/models/valida_curp.py
@@ -45,7 +45,7 @@ class ValidaCurp(models.Model):
     id_contacto = fields.Many2one('res.partner',"ID contacto",compute="getid")
     
     def getid(self):
-        return self.env.user.partner_id
+        return self.env.user.id
     
     def comprobar(self):
         for record in self:
