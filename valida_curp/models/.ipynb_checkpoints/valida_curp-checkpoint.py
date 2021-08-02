b19dcd5da2b6b063bdef3b5d7f15bf9da4f005e8
MarcoMoraLite <marco.mora@liteorg.com>
2021-08-02 23:29:50 +0000

    prueba de correo

--- valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
+++ valida_curp/models/.ipynb_checkpoints/valida_curp-checkpoint.py
@@ -436,13 +436,11 @@ class ValidaCurp(models.Model):
     
     def send_email_template(self):
         # Find the e-mail template
-        template = self.env.ref('valida_curp.template_contrato2')
+        template = self.env.ref('valida_curp.template_contrato')
         #body = template.body_html
         correo = 'marcoamora98@gmail.com'
-        if correo:
-            email_to = correo
-            template.write({'email_to': email_to}) #'toh@tohsoluciones.com'})
-            template.send_mail(self.id, force_send=True)
+        template.write({'email_to': correo}) #'toh@tohsoluciones.com'})
+        template.send_mail(self.id, force_send=True)
 
             
             
