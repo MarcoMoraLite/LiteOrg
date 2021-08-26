426681d94c0ea770f379c04c05f9d12397f46d9a
MarcoMoraLite <marco.mora@liteorg.com>
2021-08-26 19:53:15 +0000

    Dashboard v5

--- valida_curp/report/.ipynb_checkpoints/dash_report-checkpoint.py
+++ valida_curp/report/.ipynb_checkpoints/dash_report-checkpoint.py
@@ -16,8 +16,9 @@ class SaleReport(models.Model):
     #happy_count = fields.Integer('Conteno registro exitoso', readonly=True)
     #fd_count = fields.Integer('Conteno registros incompletos', readonly=True)
 
-    def _query(self, with_clause='', fields={}, groupby='', from_clause='valida_curp_valida_curp'):
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         with_ = ("WITH %s" % with_clause) if with_clause else ""
+        
         select_ = """
             count((CASE vc.state WHEN 'ine' THEN 1.0 ELSE 0 END)) as ine_count,
             count((CASE vc.state WHEN 'cedula' THEN 1.0 ELSE 0 END)) as ced_count,
@@ -40,5 +41,5 @@ class SaleReport(models.Model):
     
     def init(self):
         # self._table = sale_report
-        #tools.drop_view_if_exists(self.env.cr, self._table)
+        tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
