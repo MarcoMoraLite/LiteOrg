1ed6a299f17fd166e153585bc52f47f87ac5a9fb
MarcoMoraLite <marco.mora@liteorg.com>
2021-09-23 17:38:25 +0000

    Cambios dashboard 22 rechazado

--- valida_curp/report/.ipynb_checkpoints/dash_report-checkpoint.py
+++ valida_curp/report/.ipynb_checkpoints/dash_report-checkpoint.py
@@ -27,6 +27,7 @@ class SaleReport(models.Model):
     rechazo_registro = fields.Integer('Registros rechazados', readonly=True)
     rechazo_registro_aux = fields.Integer('Registros rechazados', readonly=True)
     fecha = fields.Datetime('Fecha de registro',readonly=True)
+    ine_doc = fields.Binary("INE delantera")
 
     
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
@@ -50,7 +51,8 @@ class SaleReport(models.Model):
             count(*) as total_registros,
             vc.create_date as fecha,
             count((CASE vc.estatus_gen WHEN 'Rechazado' THEN 1.0 ELSE 0 END)) as rechazo_registro,
-            (CASE vc.estatus_gen WHEN 'Rechazado' THEN 1.0 ELSE 0 END) as rechazo_registro_aux
+            (CASE vc.estatus_gen WHEN 'Rechazado' THEN 1.0 ELSE 0 END) as rechazo_registro_aux,
+            vc.ine as ine_doc
         """
         for field in fields.values():
             select_ += field
